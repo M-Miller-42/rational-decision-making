77f8a5e26bdef2856938d231d6f65a23f3cd7000
Martin Miller <martin.mil@web.de>
2024-03-05 20:01:53 +0100

    Add CriteriaTable component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 import OptionsColumn from './components/OptionsColumn.vue'
+import CriteriaTable from './components/CriteriaTable.vue'
 
 const options = ref([])
+const criteria = ref([])
 </script>
 
 <template>
   <main>
     <OptionsColumn :options @added="(newValue) => options.push(newValue)" />
+    <CriteriaTable :options :criteria @added="(newValue) => criteria.push(newValue)" />
   </main>
 </template>
 
---
+++ src/components/CriteriaTable.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+defineProps({
+  criteria: Array,
+  options: Array
+})
+
+defineEmits(['added'])
+</script>
+
+<template>
+  <template v-for="criterion in criteria">
+    <div class="criterion-name">{{ criterion }}</div>
+    <div v-for="_ in options.length"> {{ criterion }}</div>
+  </template>
+  <button @click="$emit('added', 'Boolean')">+</button>
+</template>
+
+<style scoped>
+.criterion-name,
+button {
+  grid-row: 1;
+}
+</style>
