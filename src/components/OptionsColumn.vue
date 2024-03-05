202275b2efa13c31656ec157ee41b11aee336b0e
Martin Miller <martin.mil@web.de>
2024-03-05 20:00:44 +0100

    Add OptionsColumn component

---
+++ src/components/OptionsColumn.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  options: Array
+})
+
+const emit = defineEmits(['added'])
+
+function addOption(e) {
+  const value = e.target.value.trim()
+  if (value) {
+    emit('added', value)
+    e.target.value = ''
+  }
+}
+</script>
+
+
+<template>
+  <div>Options</div>
+  <div v-for="option in options">{{ option }}</div>
+  <input @keyup.enter="addOption" placeholder="New option name" />
+</template>
+
+
+<style scoped></style>
