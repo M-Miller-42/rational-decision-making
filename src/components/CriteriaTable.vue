422c9779483f0b6a01b02ec25469b649d83d00cd
Martin Miller <martin.mil@web.de>
2024-03-07 20:50:45 +0100

    Add EditCriterionDialog

--- src/components/CriteriaTable.vue
+++ src/components/CriteriaTable.vue
@@ -2,6 +2,7 @@
 import { inject, ref } from 'vue'
 import CriterionColumn from './CriterionColumn.vue'
 import NewCriterionDialog from './NewCriterionDialog.vue'
+import EditCriterionDialog from './EditCriterionDialog.vue'
 
 defineProps({
   criteria: { type: Array, required: true },
@@ -10,22 +11,24 @@ defineProps({
   order: { type: Array, required: true }
 })
 
-const showDialog = ref(false)
+const showNewDialog = ref(false)
+const showEditDialog = ref(false)
 
 const addCriterion = inject('addCriterion')
 </script>
 
 <template>
   <template v-for="criterion in criteria" :key="criterion.id">
-    <div class="criterion-name">
+    <div @click="showEditDialog = true" class="criterion-name active">
       {{ criterion.name }}
       <div class="small dimmed">{{ criterion.type }}</div>
       <div class="small dimmed">Weight:{{ criterion.weight }}</div>
     </div>
     <CriterionColumn :criterion :optionsScores :weightedOptionsScores :order />
   </template>
-  <button @click="showDialog = true" class="header">+</button>
-  <NewCriterionDialog :showDialog @close="showDialog = false" @added="addCriterion" />
+  <button @click="showNewDialog = true" class="header">+</button>
+  <NewCriterionDialog :show="showNewDialog" @close="showNewDialog = false" @added="addCriterion" />
+  <EditCriterionDialog :show="showEditDialog" :criteria @close="showEditDialog = false" />
 </template>
 
 <style scoped>
